da7dde5678fe0e01d066d5f61ed719d93b1c98eb
GabrielCAlves <gabriel0603@alu.ufc.br>
2025-08-10 18:26:10 -0300

    Adicionando projeto com o gitignore

---
+++ Module20/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //publics
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
+
+    public float speed = 1f;
+    public string tagToCheckEnemy = "Enemy";
+    public string tagToCheckEndline = "Endline";
+
+    public GameObject startScreen;
+    public GameObject endScreen;
+
+    //privates
+    private Vector3 _pos;
+    private bool _canRun;
+
+    void Update()
+    {
+        if (!_canRun)
+        {
+            return;
+        }
+
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == tagToCheckEnemy)
+        {
+            EndGame();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == tagToCheckEndline)
+        {
+            EndGame();
+        }
+    }
+
+    public void EndGame()
+    {
+        _canRun = false;
+        endScreen.SetActive(true);
+    }
+
+    public void StartToRun()
+    {
+        startScreen.SetActive(false);
+        _canRun = true;
+    }
+}
